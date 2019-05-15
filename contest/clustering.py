6296d3867c80b1f8f786c2ff082a9fffa1efc030
AfterglowU <1592835208@qq.com>
2019-05-15 12:12:08 +0800

    first algo: dbscan
    NMI 0.77

--- contest/clustering.py
+++ contest/clustering.py
@@ -1,6 +1,12 @@
 import os, sys
 import numpy as np
 from sklearn.metrics.cluster import normalized_mutual_info_score
+from sklearn.decomposition import PCA
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+import dbscan
 
 def txt_to_array(filename: str) -> [np.array, np.array]:
     fp = open(filename)
@@ -20,6 +26,14 @@ def txt_to_array(filename: str) -> [np.array, np.array]:
     return ret_array[:, 0:-1], ret_array[:, -1]
 
 
+def pretreatment(data_array: np.array) -> np.array:
+    attr_var = []
+    for i in range(len(data_array[0])):
+        attr_var.append(np.var(data_array[:, i]))
+    attr_var = np.array(attr_var)
+    return 
+
+
 if __name__ == "__main__":
 
     # Move to the directory of this files
@@ -30,12 +44,22 @@ if __name__ == "__main__":
     # 1. 读入 breast.txt, 转成矩阵
     data_array, data_labels = txt_to_array("./breast.txt")
 
-    '''
-    # 2. 对 dataset 聚类, 返回 1*n 类别矩阵
-    result = clustering(data_array)
-    '''
+    # 2.1. 预处理
+    # pretreatment(data_array)
 
-    # 3. 调库计算 NMI, 开奖
-    nmi = normalized_mutual_info_score(result, data_labels)
-    print("NMI is", nmi)
+    # 2.2. 聚类
+    result_labels = dbscan.dbscan(data_array, 4.5, 50)
     
+    # 3. 调库计算 NMI
+    nmi = normalized_mutual_info_score(result_labels, data_labels)
+    print("NMI is", nmi)
+
+    # 4. PCA 后看一下图像
+    fig = plt.figure()
+    pca = PCA(n_components=3)
+    data_pca = pca.fit_transform(data_array[:, 0:-1])
+    data_labels_plot = fig.add_subplot(121, projection = '3d')
+    data_labels_plot.scatter(data_pca[:, 0], data_pca[:, 1], data_pca[:, 2], s=5, c=data_labels)
+    result_labels_plot = fig.add_subplot(122, projection = '3d')
+    result_labels_plot.scatter(data_pca[:, 0], data_pca[:, 1], data_pca[:, 2], s=5, c=result_labels)
+    plt.show()
---
+++ contest/dbscan.py
@@ -0,0 +1,56 @@
+import numpy as np
+import random
+
+def get_neighbour(data_array:np.array, center:np.array, eps:float) -> list:
+    ret = set()
+    for i in range(data_array.shape[0]):
+        if np.linalg.norm(center-data_array[i]) < eps:
+            ret.add(i)
+    return ret
+
+def dbscan(data_array:np.array, eps:float, minpoints:int) -> np.array:
+    core_objects = {}   # key  : index of this core object in data_array
+                        # value: A set of indices of objects in it's epsilon-neighbourhood
+
+    C = {}              # key  : class label
+                        # value: A set of indices of objects that belong to this class
+
+    n = data_array.shape[0]
+
+    # Find all core objects
+    for i in range(n):
+        neighbour = get_neighbour(data_array, data_array[i], eps)
+        if len(neighbour) >= minpoints:
+            core_objects[i] = neighbour
+    
+    k = 0
+    not_visited = {i for i in range(n)}
+    while core_objects:
+        not_visited_old = not_visited.copy()
+        # Pick one core object randomly
+        cores = list(core_objects.keys())
+        rand = random.randint(0,len(cores))
+        core = cores[rand]
+        queue = [core]
+        not_visited.remove(core)
+        while queue:
+            q = queue[0]
+            del queue[0]
+            if q in core_objects:
+                delta = core_objects[q] & not_visited
+                queue.extend([i for i in delta])
+                not_visited -= delta
+        # Generate cluster k
+        k += 1
+        C[k] = not_visited_old - not_visited
+        for i in C[k]:
+            if i in core_objects.keys():
+                del core_objects[i]
+        
+    # Return a list of labels rather than clustering partition C
+    labels = [-1 for i in range(n)]
+    for i in range(1,k+1):
+        for j in C[i]:
+            labels[j] = i
+
+    return labels
